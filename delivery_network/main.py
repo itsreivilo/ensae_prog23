5bd64202b9e79983be000ba1df46d0647c29f4ad
opinatel <olivier.pinatel@ensae.fr>
2023-03-16 09:55:59 +0000

    Légères modifications

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -7,10 +7,10 @@ data_path = "input/"
 file_name = "network.04.in"
 
 g = graph_from_file(data_path + file_name)
-print(g)
+# print(g)
 
-print(Graph.connected_components(g))
+# print(Graph.connected_components(g))
 
-print(Graph.get_path_with_power(g, 1, 3, 50))
+# print(Graph.get_path_with_power(g, 1, 3, 50))
 
 print(Graph.min_power(g, 1, 4))
