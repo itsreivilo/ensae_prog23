32fca1ba15af6d7c5f8a091e1d1f264b0bdcc74a
louisreisse <louis.reisse@ensae.fr>
2023-04-06 13:48:12 +0000

    t

--- delivery_network/18.py
+++ delivery_network/18.py
@@ -1,70 +1,89 @@
 #Question 18
-from graph import Graph, graph_from_file
+from graph import Graph, graph_from_file, min_power_opti
 from operator import itemgetter
+from tqdm import tqdm
+
 #Première idée : algorithme glouton
 
 # On commence par associer à chaque trajet le profit rapporté 
-j = graph_from_file('network.1.in')
-
-g = open('routes.1.in', 'r')
-L = []
-n = g.readline()
-
-for _ in range(n):
-    edge = list(map(int, g.readline().split()))
-    src, dst, util = edge
-    L.append(((src,dst),util))
-    if len(edge) != 3:
-        raise Exception("Format incorrect")
-
-# L est la liste des trajets et de l'utilité associée
+g_18 = graph_from_file('input/network.1.in')
 
 
-h = open('trucks.1.in', 'r')
-C = []
-m = h.readline()
+with open('input/routes.3.in', "r") as file:
 
-for _ in range(m):
-    edge = list(map(int, h.readline().split()))
-    power, cout = edge
-    L.append((power, cout))
-    if len(edge) != 2:
-        raise Exception("Format incorrect")
+    L = []
+    [n] = list(map(int, file.readline().split()))
+    
+    
 
-# C est la liste des coûts et de la puissance
+    for _ in range(n):
+        edge = list(map(int, file.readline().split()))
+        if len(edge) == 3:
+            src, dst, util = edge
+            L.append(((src,dst),util))
+        if len(edge) == 4:
+            src, dst, util, l = edge
+            L.append(((src,dst),util))
 
-'''for each trajet 
-on cherche la puissance min necessaire
-on cherche le camion le moins cher avce au moins cette puissance et on lajoute
-on se retrouvera avec une liste de tuples (trajet, utilite, cout)
-profit = utilite - cout
-liste de tuples (trajet, utilite, cout, profit)'''
+    # L est la liste des trajets et de l'utilité associée
 
-L_Sort = sorted(L, key=itemgetter(1), reverse=True)
-# On trie les trajets par ordre décroissant de profit
 
-C_Sort = sorted(C, key=itemgetter(0,1))
-# On trie les camions par ordre croissant de puissance puis par ordre croissant de coût
+    h = open('input/trucks.0.in', 'r')
+    C = []
+    [m] = list(map(int, h.readline().split()))
 
-B = 25*(10**9)
-# B est le budget de l'entreprise
+    for _ in range(m):
+        edge = list(map(int, h.readline().split()))
+        if len(edge) == 2:
+            power, cout = edge
+            C.append((power, cout))
+        if len(edge) == 3:
+            power, cout, l = edge
+            C.append((power, cout))
 
-R = []
-# R sera la liste des camions à acheter associés à leur trajet respectif
+    # C est la liste de la puissance et des couts
 
-Pi = 0
-# Pi sera le profit total
+    '''for each trajet 
+    on cherche la puissance min necessaire
+    on cherche le camion le moins cher avec au moins cette puissance et on l'ajoute
+    on se retrouvera avec une liste de tuples (trajet, utilite, cout)
+    profit = utilite - cout
+    liste de tuples (trajet, utilite, cout, profit)'''
 
-for i in range(len(L_Sort)) :
-    puis = Graph.min_power(self, L_Sort[i][0])
-    J = []
-    while J == []:
-        for j in range(len(C_Sort)):
-            if C_Sort[j][0] >= puis:
-                J.append(C_Sort[j][0])
-    if J[0][1] <= B:
-        B = B - J[0][1]
-        R.append((L_Sort[i][0], C_Sort[j]))
-        Pi += J[0][0]
+    L_Sort = sorted(L, key=itemgetter(1), reverse=True)
+    
+    # On trie les trajets par ordre décroissant d'utilité
 
-print('La liste des camions à acheter est : ', R,'\n Le profit total ainsi réalisé par l\'entreprise sera de', Pi, 'et son budget restant sera de', B)
+    C_Sort = sorted(C, key=itemgetter(0,1))
+    # On trie les camions par ordre croissant de puissance puis par ordre croissant de coût
+    
+    B = 25*(10**9)
+    # B est le budget de l'entreprise
+
+    R = []
+    # R sera la liste des camions à acheter associés à leur trajet respectif
+
+    Pi = 0
+    # Pi sera le profit total
+
+    for i in tqdm(range(len(L_Sort))):
+        puis = min_power_opti(g_18, L_Sort[i][0])
+        if puis == None:
+            continue
+        J = []
+        while J == []:
+            for j in range(len(C_Sort)):
+            
+                if C_Sort[j][0] >= puis:
+                    
+                    A = list(C_Sort[j:len(C_Sort)])
+                    A = sorted(A, key=itemgetter(1))
+
+                    J.append(A[0])
+                    if J[0][1] <= B:
+                        B = B - J[0][1]
+                        R.append((L_Sort[i][0], C_Sort[j]))
+                        Pi += L_Sort[i][1] - J[0][1]
+    
+
+    print('La liste des camions à acheter est : ', R,'\n Le profit total ainsi réalisé par l\'entreprise sera de', Pi, 'et son budget restant sera de', B)
